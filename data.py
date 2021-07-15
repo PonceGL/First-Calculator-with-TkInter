b912cb8ea067df65eb100dce6d6f6fd58a24fd62
PonceGL <poncianogl@hotmail.com>
2021-07-15 10:55:50 -0500

    Menu Nav right

--- data.py
+++ data.py
@@ -1,3 +1,3 @@
 SECTIONS = ["Projects", "Clientes", "Budgets", "Suppliers", "Billing"]
 
-OPTIONS = ["Option One", "Option Twe", "Option Three", "Option Four", "Option Five"]
+OPTIONS = ["Option One", "Option Twe", "Option Three", "Option Four", "Option Five", "Option Six", "Option Seven", "Option Eight", "Option Nine", "Option Ten",]
--- index.py
+++ index.py
@@ -34,24 +34,39 @@ class Root(tk.Tk):
         font.nametofont("TkDefaultFont")
         self.title("Una aplicación con TkInter")
         self.columnconfigure( 0, weight=1)
-        self.columnconfigure( 1, weight=30)
+        self.columnconfigure( 1, weight=20)
         self.rowconfigure(0, weight=1)
-        self.rowconfigure(1, weight=100)
+        self.rowconfigure(1, weight=10)
+
+        #Menu Bar
+        menu_bar = tk.Menu(self)
+        
+        filemenu = tk.Menu(menu_bar, tearoff=0, title="PonceGL")
+        filemenu.add_command(label="Nuevo",)
+        filemenu.add_command(label="Abrir",)
+
+        menu_bar.add_cascade(label="PonceGL", menu=filemenu)
 
         #Header
         header = tk.Frame(self, bg=COLORS["blue"])
         header.grid(column=0, row=0, columnspan=2, rowspan=1, sticky="NSWE")
         header.columnconfigure( 0, weight=1)
-        header.columnconfigure( 1, weight=10)
+        header.columnconfigure( 1, weight=40)
+        header.columnconfigure( 2, weight=10)
+        header.columnconfigure( 3, weight=10)
+        header.columnconfigure( 4, weight=10)
+        header.columnconfigure( 5, weight=10)
+        header.rowconfigure(0, weight=1)
         
 
         # Logo
         logo = tk.Label(header, text="Nombre", fg=COLORS["yellow"], bg=COLORS["blue"], font=("Roboto", 18, "bold"))
-        logo.grid(column=0, row=0, sticky="EW")
+        logo.grid(column=0, row=0, columnspan=1, sticky="NSWE")
 
         #Menu Nav
         menu_nav = tk.Frame(header, bg=COLORS["yellow"])
-        menu_nav.grid(column=1, row=0, columnspan=1, sticky="E")
+        menu_nav.grid(column=1, row=0, columnspan=5, sticky="NSE")
+        menu_nav.rowconfigure(0, weight=1)
         
 
         def create_menu_sections():
@@ -60,7 +75,7 @@ class Root(tk.Tk):
                 header.columnconfigure(number_column, weight=1)
                 menu_buttons = tk.Button(menu_nav, text=element)
                 menu_buttons.config(bg=COLORS["blue"], highlightbackground=COLORS["blue"], fg=COLORS["light_gray"], font=("Roboto", 12), relief="flat", cursor="hand2", activeforeground=COLORS["yellow"])
-                menu_buttons.grid(column=number_column, row=0, columnspan=1, sticky="NSWE")
+                menu_buttons.grid(column=number_column, row=0, columnspan=1, sticky="NS")
                 menu_buttons.bind("<Enter>", on_enter)
                 menu_buttons.bind("<Leave>", on_leave)
                 number_column = number_column + 1
